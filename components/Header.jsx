829f1dc8d4125b380a250a195706111133bf4c9a
thejasbshetty <thejasbshetty@gmail.com>
2024-10-30 22:53:39 +0530

    Updated header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,44 @@
-import { SignedOut, SignedIn, SignInButton, UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
+import UserMenu from "./user-menu";
 
 const Header = () => {
   return (
-    <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </div>
-  )
-}
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo.png"}
+            alt="SyncWave Logo"
+            width={200}
+            height={56}
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link href="/project/create">
+            <Button className="flex items-center gap-2">
+              <PenBox size={18} />
+              <span>Create Project</span>
+            </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding">
+              <Button varient="outline">Sign-In</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            {" "}
+            <UserMenu />
+          </SignedIn>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
