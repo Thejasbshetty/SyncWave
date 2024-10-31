ef37b8e34b9ab84e8e6c3691a6055365c5251d87
thejasbshetty <thejasbshetty@gmail.com>
2024-10-31 22:31:44 +0530

    Add project retrieval and sprint creation functionalities; implement project layout

---
+++ app/(main)/project/[projectId]/layout.js
@@ -0,0 +1,13 @@
+import { Span } from 'next/dist/trace'
+import React, { Suspense } from 'react'
+import { BarLoader } from 'react-spinners'
+
+const ProjectLayout = async ({children}) => {
+  return (
+    <div className="mx-auto">
+      <Suspense fallback={<span>Loading Project...</span>}>{children}</Suspense>
+    </div>
+  )
+}
+
+export default ProjectLayout
--- app/(main)/project/[projectId]/page.jsx
+++ app/(main)/project/[projectId]/page.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { getProject } from "@/actions/projects";
+import { notFound } from "next/navigation";
+import SprintCreationForm from "../_components/create-sprint.jsx";
+import SprintBoard from "../_components/sprint-board.jsx/index.js";
 
-const ProjectPage = () => {
+export default async function ProjectPage({ params }) {
+  const { projectId } = params;
+  const project = await getProject(projectId);
+
+  if (!project) {
+    notFound();
+  }
+
   return (
-    <div>
-      ProjectPage
-    </div>
-  )
-}
+    <div className="container mx-auto">
+      <SprintCreationForm
+        projectTitle={project.name}
+        projectId={projectId}
+        projectKey={project.key}
+        sprintKey={project.sprints?.length + 1}
+      />
 
-export default ProjectPage
+      {project.sprints.length > 0 ? (
+        <SprintBoard
+          sprints={project.sprints}
+          projectId={projectId}
+          orgId={project.organizationId}
+        />
+      ) : (
+        <div>Create a Sprint from button above</div>
+      )}
+    </div>
+  );
+}
